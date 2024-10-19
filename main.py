8326511789d946b3f5a4bb030ab1b016b0e6008a
Mohit Kumar <mk736932@gmail.com>
2024-10-19 20:59:40 +0530

    till contact

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles  # Import StaticFiles for serving static files
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Load templates from the 'templates' directory
+# Serve static files from the 'static' directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Load templates from the 'templates' directory (including the 'login' subdirectory)
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
@@ -14,11 +18,14 @@ async def index(request: Request):
 
 @app.get("/login")
 async def login(request: Request):
-    # Render the login.html template
-    return templates.TemplateResponse("login.html", {"request": request})
+    # Render the login.html template from the 'login' folder inside 'templates'
+    return templates.TemplateResponse("login/login.html", {"request": request})
+
+@app.get("/contact")
+async def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
 
 @app.post("/redirect_to_login")
 async def redirect_to_login():
     # Redirect from index.html to login.html
     return RedirectResponse(url="/login")
-
